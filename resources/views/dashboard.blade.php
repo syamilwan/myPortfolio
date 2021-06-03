e10e402abb2a504b655d3f21dd1ea802448312c3
syamilwan <syamil.wan46@gmail.com>
2021-06-03 12:12:07 +0800

    dashboard--

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container my-5 py-4 text-center">
-        <div class="d-flex justify-content-center space-between border-bottom">
-            <a href="/" class="d-flex align-items-center pb-2 my-3 link-dark text-decoration-none">
-                <img class="rounded-circle thumbnail-box-shadow" src="{{ URL::asset('image/default-profile-pic.jpg')}}" alt="profile picture" width="120" height="120"/> 
-                <span class="fs-1 fw-semibold">&nbsp; Hi!</span>
-            </a>
+<div class="row">
+    <div class="col-12">
+        <div class="card bg-light">
+            <div class="card-header border-bottom">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="fs-3 fw-semibold">Dashboard</div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="fs-5 fw-semibold">Profile Details</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="fs-5 fw-semibold">Academic</div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="fs-5 fw-semibold">Skills</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <br><span class="fs-5 fw-semibold">Dashboard</span>
-
     </div>
+</div>
 @endsection
